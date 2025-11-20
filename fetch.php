e726601ef4765278f178ffb1d9610d1df6ddeb3b
rayraval1811 <rayraval1811@gmail.com>
2025-11-20 16:24:32 +0530

    Add files via upload

--- fetch.php
+++ fetch.php
@@ -9,6 +9,7 @@ if($query->num_rows > 0){
     <table border>
         <th>ID</th>
         <th>First Name</th>
+        <th>Action</th>
     
     <?php
     while($row = $query->fetch_assoc()){
@@ -16,6 +17,13 @@ if($query->num_rows > 0){
             <tr>
                 <td><?php echo $row['table_id']; ?></td>
                 <td><?php echo $row['table_fname']; ?></td>
+                <td>
+                    <form action="update.php" method='POST'>
+                        <input type="hidden" name="fetch_id" value="<?php echo $row['table_id']; ?>">
+                        <input type="submit" name= "Update_btn" value ="Update">
+                    </form>
+                    <input type="submit" value ="Delete">
+                </td>
             </tr>
        <?php
         

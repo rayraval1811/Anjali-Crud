82e8d58d92c7ab4f0763e5f6a8b76f967f7d1c62
rayraval1811 <rayraval1811@gmail.com>
2025-12-17 16:40:56 +0530

    Update updatee.php

--- updatee.php
+++ updatee.php
@@ -33,16 +33,39 @@ if(isset($_POST['update_btn'])){
 <body>
 
     <h1>Update Form</h1>
-    <form action="update_procces.php" method="POST">
+    <form action="update_procces.php" method="POST" enctype="multipart/form-data">
 
         <label for="">ID</label>
-        <input type="text" name="reg_id" value="<?php echo $row['table_id']; ?>"><br><br>
+        <input type="text" name="update_id" value="<?php echo $update_id; ?>"><br><br>
 
         <label for="">First Name</label>
-        <input type="text" name="reg_fname" value="<?php echo $row['table_fname']; ?>"><br><br>
+        <input type="text" name="update_fname" value="<?php echo $row['table_fname'];?>"><br><br>
 
         <label for="">Email</label>
-        <input type="text" name="reg_email"><br><br>
+        <input type="text" name="update_email" value="<?php echo $row['table_email'];?>"><br><br>
+
+        <label for="">Password</label>
+        <input type="password" name="update_password" value="<?php echo $row['table_password'];?>"><br><br>
+
+        <label>Gender</label><br>
+        <input type="radio" name="update_gender" value="Male"
+            <?php if($row['table_gender']=="Male"){ echo "checked"; } ?>> Male
+
+        <input type="radio" name="update_gender" value="Female"
+            <?php if($row['table_gender']=="Female"){ echo "checked"; } ?>> Female
+
+        <input type="radio" name="update_gender" value="Other"
+            <?php if($row['table_gender']=="Other"){ echo "checked"; } ?>> Other
+        <br><br>
+
+        <label>Current Photo</label><br>
+        <img src="<?php echo $row['table_img']; ?>" width="100" height="100"><br><br>
+
+        <label>Change Photo</label>
+        <input type="file" name="update_img"><br><br>
+
+        <input type="hidden" name="old_img" value="<?php echo $row['table_img']; ?>">
+
 
 
         <input type="submit" name="update_btn" value="Update">

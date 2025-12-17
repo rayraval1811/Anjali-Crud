4528ffc9cc3d26da3876cd8afc40b40b96e3ce32
rayraval1811 <rayraval1811@gmail.com>
2025-12-17 16:41:21 +0530

    Update update_procces.php

--- update_procces.php
+++ update_procces.php
@@ -6,15 +6,31 @@ include('Connection.php');
 if(isset($_POST['update_btn'])){
     // echo "btn set";
 
-    $update_id = $_POST['reg_id'];
+    $update_id = $_POST['update_id'];
+    $update_name = $_POST['update_fname'];
+    $update_email = $_POST['update_email'];
+    $update_password = $_POST['update_password'];
+    $update_gender = $_POST['update_gender'];
 
-    $update_name = $_POST['reg_fname'];
+
+
+
+    $new_img = $_FILES['update_img']['name'];
+    $tmp_img = $_FILES['update_img']['tmp_name'];
+    $path = "upload/".$new_img;
+
+    move_uploaded_file($tmp_img, $path);
 
     // echo $update_name;
 
 
-    $update = "UPDATE `anjali_table` SET `table_fname`='$update_name'
-     WHERE `table_id`= '$update_id'";
+    $update = "UPDATE `anjali_table` SET 
+        `table_fname`    = '$update_name',
+        `table_email`    = '$update_email',
+        `table_password` = '$update_password',
+        `table_gender`   = '$update_gender',
+        `table_img`      = '$path'
+        WHERE `table_id` = '$update_id'";
     $query= mysqli_query($conn, $update);
 
     if($query){
